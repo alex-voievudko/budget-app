951a35fd294bf729656757c44adc247024294116
Alex Voievudko <alex.voievudko@gmail.com>
2022-08-08 19:10:52 +0200

    Added BudgetCard component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import React from 'react';
-import { ChakraProvider, Box, Grid, theme } from '@chakra-ui/react';
+import {
+  ChakraProvider,
+  Box,
+  Grid,
+  theme,
+  HStack,
+  Heading,
+  Button,
+} from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
+import BudgetCard from './components/BudgetCard';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
+      <Box px={6} my={6} fontSize="xl">
+        <HStack spacing={2} mb={4}>
+          <Heading mr="auto" as="h1" size="lg">
+            Budgets
+          </Heading>
+          <Button colorScheme="teal" variant="solid">
+            Add Budget
+          </Button>
+          <Button colorScheme="teal" variant="outline">
+            Add Expense
+          </Button>
+        </HStack>
+        <Grid
+          templateColumns="repeat(auto-fill, minmax(300px, 1fr))"
+          gap="1rem"
+          alignItems="flex-start"
+        >
+          <BudgetCard name="Entertainment" amount="800" max="1000"></BudgetCard>
         </Grid>
       </Box>
     </ChakraProvider>
@@ -15,3 +39,12 @@ function App() {
 }
 
 export default App;
+
+{
+  /* <Grid minH="100vh" p={3}>
+          <ColorModeSwitcher justifySelf="flex-end" />
+          <HStack spacing="2" mb="4">
+            <Heading>Budgets</Heading>
+          </HStack>
+        </Grid> */
+}
---
+++ src/components/BudgetCard.js
@@ -0,0 +1,46 @@
+import { Box, Heading, Stack, Text, Progress, Button } from '@chakra-ui/react';
+import { currencyFormatter } from '../utils/currencyFormatter';
+
+const getProgressBarColor = (amount, max) => {
+  const ratio = amount / max;
+  if (ratio < 0.5) return 'teal';
+  if (ratio < 0.75) return 'yellow';
+  return 'red';
+};
+
+const BudgetCard = ({ name, amount, max }) => {
+  return (
+    <Box p={6} w="full" boxShadow="md" borderWidth={1}>
+      <Stack
+        direction="row"
+        justifyContent="space-between"
+        alignItems="baseline"
+        mb={3}
+      >
+        <Heading as="h2" size="md">
+          {name}
+        </Heading>
+        <Text fontSize="md" display="flex" alignItems="baseline">
+          {currencyFormatter.format(amount)} /
+          <Text as="span" fontSize="sm" color="gray.600" ms={1}>
+            {currencyFormatter.format(max)}
+          </Text>
+        </Text>
+      </Stack>
+      <Progress
+        colorScheme={getProgressBarColor(amount, max)}
+        min={0}
+        max={max}
+        value={amount}
+      />
+      <Stack direction="row" gap="2" mt="4">
+        <Button colorScheme="teal" variant="outline" ms="auto">
+          Add Expense
+        </Button>
+        <Button variant="outline">View Expenses</Button>
+      </Stack>
+    </Box>
+  );
+};
+
+export default BudgetCard;
